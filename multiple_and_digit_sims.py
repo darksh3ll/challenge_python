1532447be9d07538e94a42da74a453416b6a02de
stephane <darksh3ll.js@gmail.com>
2021-12-05 11:00:02 +0100

    refactoring code

--- multiple_and_digit_sims.py
+++ multiple_and_digit_sims.py
@@ -31,6 +31,13 @@ def multiple(mutiple):
         result.append(sum([int(x) for x in str(i)]))
     return sum(result)
 
+# def multiple(mutiple):
+#     foo = [i for i in [x for x in range(1, 100 + 1) if x % mutiple == 0]]
+#     print(foo)
+
+
+def procedure(n):
+    return sum(int(d) for i in range(n, 101, n) for d in str(i))
+
 
 print(multiple(12))
-
