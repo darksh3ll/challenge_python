03b460336050460f66bcc8dccff596fd5a4098f5
stephane <darksh3ll.js@gmail.com>
2021-12-16 19:00:28 +0100

    wip

---
+++ labs.py
@@ -0,0 +1,13 @@
+import json
+BODY_PRIMARY = {"deviceid": "", "data": {}}
+
+status = "on"
+BODY_PRIMARY["data"] = dict({"switch": status})
+
+print(BODY_PRIMARY)
+
+a = [12,23,47]
+
+foo = [x for x in a if x %2 == 1]
+print(foo)
+
