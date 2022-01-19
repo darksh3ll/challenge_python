d9415bab77e72db8c608facca0e79b26da70bed4
stephane <darksh3ll.js@gmail.com>
2022-01-19 18:50:28 +0100

    wip

--- get_names.py
+++ get_names.py
@@ -1,28 +1,12 @@
-data = [
-    {'name': 'Joe', 'age': 20},
-    {'name': 'Bill', 'age': 30},
-    {'name': 'Kate', 'age': 23}
-]
+data = {
+    0: {
+        "prenom": "julie",
+        "profession": "developpeuse",
+    },
+    1: {
+        "prenom": "stephane",
+        "profession": "plombier",
+    }
+}
 
-
-def itemgetter(item):
-    return item['name']
-
-
-def get_names(data):
-    return list(map(itemgetter, data))
-
-
-# print(get_names(data))
-
-
-num1 = [1, 2, 3]
-num2 = [5,6,7]
-
-
-def adition_array(number):
-    return number %2 == 0
-
-
-foor = list(filter(adition_array, num1))
-print(foor)
+print(data[0])
