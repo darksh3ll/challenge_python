aceb64a6a1950a34366349f0418cf2cdd5400b16
stephane <darksh3ll.js@gmail.com>
2022-08-11 20:48:51 +0200

    wip

--- algo_perso/pendu.py
+++ algo_perso/pendu.py
@@ -3,9 +3,16 @@ def modify_multiply(st, loc, num):
     return foo[:-1]
 
 
-modify_multiply("This is a string", 3, 5)
+# modify_multiply("This is a string", 3, 5)
 
 
 # solution code ware
 def modify_multiply(st, loc, num):
     return '-'.join([st.split()[loc]] * num)
+
+
+def solution(n):
+    return 7
+
+
+print(solution(4))
